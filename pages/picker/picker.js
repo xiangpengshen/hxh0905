f46801987819889facca8215c5f4c762c3785978
sxp <sxp@huaxianhui.cn>
2019-09-06 10:34:37 +0800

    update
    update

--- pages/picker/picker.js
+++ pages/picker/picker.js
@@ -5,7 +5,7 @@ const app = getApp()
 Page({
   data: {
     title: '选择器',
-    region: ['广东省', '广州市', '海珠区'],
+    region: ['广东省1', '广州市2', '海珠区3'],
   },
   bindTimeChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
